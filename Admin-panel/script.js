426946f60414bc7b3bb5f915dd465426e6401a89
leandreAlly <tuyambazeleandre@gmail.com>
2022-01-20 12:42:16 +0200

    new script added

--- Admin-panel/script.js
+++ Admin-panel/script.js
@@ -1,5 +1,17 @@
 let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".sidebarBtn");
+
 sidebarBtn.onclick = function() {
   sidebar.classList.toggle("active");
 }
+
+const fromArticle = document.querySelector(".form-container");
+const showfromArticleBtn = document.querySelector("#add-article");
+const showDashbord = document.querySelector("#dashbord-show");
+
+showfromArticleBtn.addEventListener('click', () => {
+     fromArticle.style.display = "block"; 
+});
+showDashbord.addEventListener('click', () => {
+  fromArticle.style.display = "none"; 
+})
